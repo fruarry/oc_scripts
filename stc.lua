599ac61f333f3cd4eaeee94ae54a15cf4a31033a
fruarry <76531213+fruarry@users.noreply.github.com>
2024-10-06 11:20:59 +0800

    update stc

--- stc.lua
+++ stc.lua
@@ -1,32 +1,45 @@
 local computer = require("computer")
 local component = require("component")
 local os = require("os")
+local string = require("string")
 local sides = require("sides")
 
 local solar_tower = component.gt_machine
-local transposer = nil
+local transposer = component.transposer
+local coolant_tank_side = sides.east
+local input_side = sides.bottom
 
+local heat_threshold = 50000
+
+-- transfer coolant from tank to fluid input hatch
 function transfer_coolant(amount)
+    local status, xfer_amount = transposer.transferFluid(coolant_tank_side, input_side, amount)
 end
 
-function parse(sensor_info)
-    return {}
+-- example sensor information of solar tower
+-- {"solartower.controller.tier.single", "Internal Heat Level: 100000", ...}
+-- return heat
+function parse_heat(sensor_info)
+    return tonumber(string.sub(sensor_info[2], 22))
 end
 
 function main()
-    if solar_tower == nil then
-        print("Solar tower not found.")
-        return
-    end
-
     while true do
-        local raw_info = solar_tower.getSensorInformation()
-        info = parse(raw_info)
-
-        -- coolant transfer when internal heat is higher than 50000
-        if info.heat > 50000 then
-            transfer_coolant(info.heat - 50000)
+        local status, ret = pcall(solar_tower.getSensorInformation)
+        if not status then
+            print("Unable to obtain solar tower sensor information.")
+            goto continue
         end
+        local heat = parse_heat(ret)
+
+        -- coolant transfer when internal heat is higher than heat_threshold
+        if heat > heat_threshold then
+            transfer_coolant(heat - heat_threshold)
+        end
+
+        ::continue::
+        -- sleep to reduce resource usage
+        os.sleep(1)
     end
 end
 
